844f9cd8c4e47ad685c3a0bcc23fbc53bcef4dc7
Ulises Bojorquez Ortiz <ulibojorquez@gmail.com>
2022-07-01 16:10:04 -0600

    Add users to Firebase

--- web_admin_dashboard/src/pages/new/New.jsx
+++ web_admin_dashboard/src/pages/new/New.jsx
@@ -2,10 +2,34 @@ import React, { useState } from 'react';
 import "./new.scss";
 import { Navbar, Sidebar } from '../../components';
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
+import { addDoc, collection, doc, serverTimestamp, setDoc } from "firebase/firestore"; 
+import { auth,db } from '../../firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 
 const New = ({inputs, title}) => {
     const [file, setFile] = useState("");
-    console.log(file)
+    const [data,setData] = useState({});
+
+    const handleAdd = async (e) =>{
+        e.preventDefault();
+        try {
+            const res = await createUserWithEmailAndPassword(auth, data.email, data.password);
+            await setDoc(doc(db, "users", res.user.uid), {
+                ...data,
+                timeStamp: serverTimestamp()
+            });
+
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const handleInput = (e) =>{
+        const id = e.target.id;
+        const value = e.target.value;
+        setData({...data, [id]:value})
+    };
 
     return (
         <div className='app__new'>
@@ -22,7 +46,7 @@ const New = ({inputs, title}) => {
                         <img src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" } alt="" />
                     </div>
                     <div className="app__new-container-bottom-right">
-                        <form >
+                        <form onSubmit={handleAdd} >
                             <div className="app__new-formInput">
                                 <label htmlFor='file'> Image <DriveFolderUploadOutlinedIcon className='app__new-formInput-icon'/></label>
                                 <input type="file" id='file' onChange={e => setFile(e.target.files[0])} style={{display:"none"}}/>
@@ -31,11 +55,11 @@ const New = ({inputs, title}) => {
                                 inputs.map((input) =>(
                                     <div className="app__new-formInput" key={input.id}>
                                         <label> {input.label}</label>
-                                        <input type={input.type} placeholder={input.placeholder} />
+                                        <input id={input.id} type={input.type} placeholder={input.placeholder} onChange={handleInput} />
                                     </div>
                                 ))
                             }
-                            <button>Send</button>
+                            <button type='submit'>Send</button>
                         </form>
                     </div>
                 </div>
