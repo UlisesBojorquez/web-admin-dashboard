d61952734db344b3e6a187b0a3067c03d2e29caa
Ulises Bojorquez Ortiz <ulibojorquez@gmail.com>
2022-07-02 11:46:56 -0600

    Update real time data Firebase

--- web_admin_dashboard/src/components/datatable/Datatable.jsx
+++ web_admin_dashboard/src/components/datatable/Datatable.jsx
@@ -3,26 +3,41 @@ import "./datatable.scss";
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns } from '../../datatablesource';
 import {Link} from "react-router-dom";
-import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { collection, deleteDoc, doc, onSnapshot  } from "firebase/firestore";
 import { db } from '../../firebase';
 
 const Datatable = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () =>{
+        // const fetchData = async () =>{
+        //     let list = [];
+        //     try {
+        //         const querySnapshot = await getDocs(collection(db, "users"));
+        //         querySnapshot.forEach((doc) => {
+        //             list.push({id:doc.id, ...doc.data()});
+        //         });
+        //         setData(list);
+        //     } catch (error) {
+        //         console.log(error);
+        //     }
+        // };
+        // fetchData();
+
+        //LISTEN REALTIME DATA
+        const unsub = onSnapshot(collection(db, "users"), (snapShot) => {
             let list = [];
-            try {
-                const querySnapshot = await getDocs(collection(db, "users"));
-                querySnapshot.forEach((doc) => {
-                    list.push({id:doc.id, ...doc.data()});
-                });
-                setData(list);
-            } catch (error) {
-                console.log(error);
-            }
+            snapShot.docs.forEach((doc) => {
+                list.push({id:doc.id, ...doc.data()});
+            })
+            setData(list);
+          },(error)=>{
+            console.log(error);
+          });
+
+        return () => {
+            unsub();
         };
-        fetchData();
     }, [])
     
     
